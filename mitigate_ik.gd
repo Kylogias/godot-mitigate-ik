bef2063728936da2c3af8859f45a622c9aba7c98
Kylogias <45217130+Kylogias@users.noreply.github.com>
2021-01-05 12:34:07 -0500

    Added a warn statement

--- mitigate_ik.gd
+++ mitigate_ik.gd
@@ -10,9 +10,12 @@ export var model_root := NodePath("..") # (Optional) The model's root node, usef
 
 func set_active(val):
 	active = val
+	if active and not (get_parent() is Skeleton):
+		push_warning("(from MitigateIK3D) WARNING: The script may not work properly if this node isn't a child of the model")
 	set_process(val)
 
-func _process(delta):
+
+func _bprocess(_delta):
 	var root = get_node(scene_root) as Spatial
 	var root_rotation = root.rotation
 	var model_rotation
@@ -21,11 +24,13 @@ func _process(delta):
 		mdl = get_node(model_root)
 		model_rotation = mdl.rotation
 		mdl.rotation = Vector3.ZERO
+	
 	root.rotation = Vector3.ZERO
 	var target_node = get_node(IK_target) as Spatial
 	var pos = get_transform()
 	pos.origin += root.get_global_transform().origin
 	target_node.set_global_transform(pos)
 	root.rotation = root_rotation
+	
 	if not model_root.is_empty():
 		mdl.rotation = model_rotation
