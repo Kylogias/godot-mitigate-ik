d27bad70d80babc380bf72d068bd3865dac9bfb7
Kylogias <45217130+Kylogias@users.noreply.github.com>
2021-01-22 19:35:11 -0500

    Redid the approach to mitigating
    I redid the approach to mitigating the IK scaling, which finally fixed a rotational issue

--- mitigate_ik.gd
+++ mitigate_ik.gd
@@ -4,33 +4,21 @@ extends Position3D
 
 export var IK_target := NodePath("..") # The node the SkeletonIK node targets
 export var active := false setget set_active # Whether or not this node's active
-export var scene_root := NodePath("..") # The root of the scene (Usually where movement and rotation takes place)
-export var model_root := NodePath("..") # (Optional) The model's root node, useful for if the model is rotated
 
 
 func set_active(val):
 	active = val
-	if active and not (get_parent() is Skeleton):
-		push_warning("(from MitigateIK3D) WARNING: The script may not work properly if this node isn't a child of the model")
 	set_process(val)
 
 
 func _process(_delta):
-	var root = get_node(scene_root) as Spatial
-	var root_rotation = root.rotation
-	var model_rotation
-	var mdl
-	if not model_root.is_empty():
-		mdl = get_node(model_root)
-		model_rotation = mdl.rotation
-		mdl.rotation = Vector3.ZERO
-	
-	root.rotation = Vector3.ZERO
-	var target_node = get_node(IK_target) as Spatial
-	var pos = get_transform()
-	pos.origin += root.get_global_transform().origin
+	var global_scale := get_global_transform().basis.get_scale()
+	var target_node := get_node(IK_target) as Spatial
+	var pos : Transform = get_global_transform()
+	var p_pos := get_parent_spatial().get_global_transform().origin
+	if get_parent_spatial():
+		pos.origin -= p_pos
+	pos.origin /= global_scale
+	if get_parent_spatial():
+		pos.origin += p_pos
 	target_node.set_global_transform(pos)
-	root.rotation = root_rotation
-	
-	if not model_root.is_empty():
-		mdl.rotation = model_rotation
